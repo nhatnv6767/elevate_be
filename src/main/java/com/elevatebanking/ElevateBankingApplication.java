19d510232e3a52c35098309151642209c246f147
bhnone <>
2024-11-27 02:17:29 +0900

    first commit

---
+++ src/main/java/com/elevatebanking/ElevateBankingApplication.java
@@ -0,0 +1,13 @@
+package com.elevatebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevateBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElevateBankingApplication.class, args);
+    }
+
+}
