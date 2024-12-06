e871eed16aa0bbf0acde3e0ce4a3b3e7066b597f
bhnone <>
2024-12-06 22:45:49 +0900

    dotenv deleted

--- src/main/java/com/elevatebanking/service/nonImp/EmailService.java
+++ src/main/java/com/elevatebanking/service/nonImp/EmailService.java
@@ -1,40 +1,29 @@
 package com.elevatebanking.service.nonImp;
 
-import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
     private final JavaMailSender mailSender;
-
-    @Value("${spring.mail.username}")
-    private String fromEmail;
+    private final OAuth2AuthorizedClientService authorizedClientService;
 
     public void sendPasswordResetEmail(String to, String token) {
-        try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-
-            helper.setFrom(fromEmail);
-            helper.setTo(to);
-            helper.setSubject("Password Reset Request");
-
-            String content = String.format(
-                    "To reset your password, click the link below:\n%s/reset-password?token=%s\n\n" +
-                            "If you did not request a password reset, please ignore this email.",
-                    "https://yourapp.com", token
-            );
-
-            helper.setText(content);
-            mailSender.send(message);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Error sending email", e);
-        }
+        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient("google", "user");
+        // Sử dụng client để gửi email
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("Password Reset Request");
+        message.setText(String.format(
+                "To reset your password, click the link below:\nhttps://yourapp.com/reset-password?token=%s\n\n" +
+                        "If you did not request a password reset, please ignore this email.",
+                token));
+        mailSender.send(message);
     }
 }
