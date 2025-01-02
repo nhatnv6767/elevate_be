3ab7ba01ca47ee77508e4143148f39afb35ae05c
bhnone <>
2025-01-02 22:29:33 +0900

    Use Profile DEV

--- src/main/java/com/elevatebanking/config/SecurityConfig.java
+++ src/main/java/com/elevatebanking/config/SecurityConfig.java
@@ -1,5 +1,6 @@
-package com.elevatebanking.entity.config;
+package com.elevatebanking.config;
 
+import com.elevatebanking.entity.config.SecurityProperties;
 import com.elevatebanking.security.JwtAuthenticationFilter;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.elevatebanking.security.JwtTokenProvider;
 // import com.elevatebanking.service.nonImp.CustomUserDetailsService;
 import com.elevatebanking.service.nonImp.CustomUserDetailsService;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -75,6 +81,10 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/webjars/**").permitAll()
                         .requestMatchers("/actuator/mappings/**").permitAll()
                         .requestMatchers("/reset-password/**").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
@@ -97,5 +107,18 @@ public class SecurityConfig {
                         }));
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://192.168.1.202:8080", "http://localhost:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
